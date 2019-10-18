b3be42a0f260ad3dd309124d3e04bd83e668a0c4
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-18 04:35:21 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,9 +2,8 @@ require 'pry'
 
 def my_find(collection)
   idx = 0
-  final_collection = []
   while idx < collection.length
-    final_collection << yield(collection[idx])
+    if yield(collection[idx])
   idx += 1
   end
   if final_collection.detect(true)
