2897c2260620cfcb294da3bb9c70c95b2395cfcd
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-18 04:39:07 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,19 @@
 require 'pry'
 
-def my_find(collection)
+# def my_find(collection)
+#   idx = 0
+#   while idx < collection.length
+#     if yield(collection[idx])
+#       return collection[idx]
+#     end
+#   idx += 1
+#   end
+# end
 
+def my_find(collection)
+  i = 0
+  while i < collection.length
+  return collection[i] if yield(collection[i])
+  i += 1 
+  end
 end
