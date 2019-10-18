140cf657725d2020a41dd101918904a296da193e
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-18 04:36:22 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,11 +4,8 @@ def my_find(collection)
   idx = 0
   while idx < collection.length
     if yield(collection[idx])
+      return collection[idx]
+    end
   idx += 1
-  end
-  if final_collection.detect(true)
-    true
-  else
-    false
   end
 end
